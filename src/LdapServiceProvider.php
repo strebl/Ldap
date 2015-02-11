cab1299b48128618c474aa4fc3fa5fe795eb23ea
Manuel Strebel <manuel.strebel@gibb.ch>
2015-02-11 18:07:03 +0100

    psr-4

---
+++ src/LdapServiceProvider.php
@@ -0,0 +1,50 @@
+<?php namespace Xavrsl\Ldap;
+
+use Illuminate\Support\ServiceProvider;
+
+class LdapServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->publishes([
+		    __DIR__.'/config/config.php' => config_path('ldap.php'),
+		]);
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+		$this->app['ldap'] = $this->app->share(function()
+		{
+			return new LdapManager();
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('ldap');
+	}
+
+}
